3e258387f6be818b8861389445f9390b8e9aa91b
martinotamendit <martin.otamendit@alumno.buap.mx>
2025-03-15 12:31:31 -0600

    Añadir funciones Challenge

---
+++ app.js
@@ -0,0 +1,38 @@
+let listaAmigos = [];
+let contenidoLista = ''
+
+//Función que agrega un nuevo amigo a la lista y lo muestra en pantalla.
+function agregarAmigo(){
+    let amigo = document.querySelector('#amigo').value;
+    
+    if(amigo == ''){
+        alert('Ingresa un nombre válido, por favor.');
+    }else{
+        listaAmigos.push(amigo);
+        actualizarContenidoLista(listaAmigos[listaAmigos.length-1]);
+    }
+    return;
+}
+
+//Función que sortea un amigo de la lista y lo muestra en pantalla.
+function sortearAmigo(){
+    let resultadoSorteo = document.querySelector('#resultado');
+    let amigoSorteado = Math.floor(Math.random() * listaAmigos.length);
+
+    resultadoSorteo.innerHTML = `<li>El amigo secreto sorteado es: ${listaAmigos[amigoSorteado]}.</li>`;
+    actualizarContenidoLista('');
+
+    console.log(listaAmigos[amigoSorteado])
+}
+
+//Función que actualiza el contenido de la lista que se verá en pantalla.
+function actualizarContenidoLista(elementoLista){
+    let listaVisibleHTML = document.querySelector('#listaAmigos');
+    
+    if(elementoLista == ''){
+        contenidoLista = '';  
+    }else{
+        contenidoLista += `<li>${elementoLista}</li>`;
+    }
+    listaVisibleHTML.innerHTML = contenidoLista;
+}
